5d46e6dddb62dd607a290b55fa44b1c76e83cfac
FredDietrich <frederico.machry@gmail.com>
2022-06-16 21:00:10 -0300

    add exports components

--- src/app/component/index.js
+++ src/app/component/index.js
@@ -1,4 +1,6 @@
 export { default as App } from './App';
 export { default as Value } from './Value';
 export { default as TopBar } from './TopBar';
-export { default as Main } from './Main'
+export { default as Main } from './Main';
+export { default as FormItem } from './FormItem';
+export { default as FormGrid } from './FormGrid';
