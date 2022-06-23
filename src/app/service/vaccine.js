75ef2f4f06d922e4870bc51226f6a4cc650b9711
FredDietrich <frederico.machry@gmail.com>
2022-06-23 19:11:28 -0300

    usa api para vacinas

--- src/app/service/vaccine.js
+++ src/app/service/vaccine.js
@@ -1,6 +1,3 @@
-export const getVaccinesByCriteria = criteria => {
-    //TODO Usar critérios de verdade
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve([{ id: 1, name: 'Covid-19' },{ id: 2, name: 'Hepatite B' },{ id: 3, name: 'H1N1' }]), 1000)
-    })
-}
+import Api from "../api/api"
+
+export const getAllVaccines = () => Api.get("vaccines").then(data => data);
