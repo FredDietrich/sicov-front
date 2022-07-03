973eed1a37c9b53be5b13ebb00d2dfc09c0e937d
= <=>
2022-07-03 19:47:24 -0300

    cria get com payload (wip)

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,8 +1,18 @@
-const baseUrl = 'https://sicov-back.free.beeceptor.com/'
+const baseUrl = 'https://sicov.free.beeceptor.com/'
 
 const Api = {
 
     get: (endpoint) => fetch(`${baseUrl}${endpoint}`).then(res => res.json()),
+    getWithPayload: (endpoint, data) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        fetch(`${baseUrl}${endpoint}`, options).then(res => res.json())
+    },
     post: (endpoint, data) => {
         const options = {
             method: 'POST',
