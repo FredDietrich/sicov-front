be70a21a8c9e7d2fb2b344cf87c8c0775a9758ba
= <=>
2022-07-03 19:47:13 -0300

    remove app teste

--- src/SystemRoutes.js
+++ src/SystemRoutes.js
@@ -4,14 +4,14 @@ import {
     Route,
 } from "react-router-dom";
 
-import { Value, Main, Contact, Results, Login, CreateVaccine } from "./app/component";
+import { Main, Contact, Results, Login, CreateVaccine } from "./app/component";
 
 function SystemRoutes() {
     return (
         <HashRouter>
             <Routes>
                 <Route path="/" element={<Main />} />
-                <Route path="admin" element={<Value />} />
+                {/* <Route path="admin" element={<Admin />} /> */}
                 <Route path="contact" element={<Contact />} />
                 <Route path="results" element={<Results />} />
                 <Route path="login" element={<Login />} />
