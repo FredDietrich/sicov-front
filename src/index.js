a361cec3593b88c4b7804b6670384da29c02dba5
= <=>
2022-07-06 19:48:43 -0300

    add localization provider

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <SystemRoutes />
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Provider store={store}>
+        <SystemRoutes />
+      </Provider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
